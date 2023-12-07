848956f9aaf3510e2c4ef56a17d553e92f091d36
Hanna Kavaliova <anika.kovaliova@gmail.com>
2023-12-08 00:55:37 +0100

    [add task] handle prio buttons

--- js/tasks.js
+++ js/tasks.js
@@ -1,3 +1,45 @@
+const PRIO_URG = 1;
+const PRIO_MDM = 2;
+const PRIO_LOW = 3;
+
+let taskPrio = PRIO_MDM;
+
 function createNewTask(){
-   
+    let titel = document.getElementById('task-title').value;
+    let description = document.getElementById('task-description').value;
+    let date = document.getElementById('task-date').value;
+   // let titel = document.getElementById('task-title').value;
+   // let titel = document.getElementById('task-title').value;
 }
+
+function prioButtonUpdate(pressed, btn) {
+        if(pressed){
+            document.getElementById(btn).classList.add('d-none');
+            document.getElementById('clicked-' + btn).classList.remove('d-none');
+        } else {
+            document.getElementById(btn).classList.remove('d-none');
+            document.getElementById('clicked-' + btn).classList.add('d-none');
+        }
+}
+
+function prioButtonUrgent(){
+    prioButtonUpdate(true, 'urgent-btn');
+    prioButtonUpdate(false, 'medium-btn');
+    prioButtonUpdate(false, 'low-btn');
+    taskPrio = PRIO_URG;
+}
+
+function prioButtonMedium(){
+    prioButtonUpdate(false, 'urgent-btn');
+    prioButtonUpdate(true, 'medium-btn');
+    prioButtonUpdate(false, 'low-btn');
+    taskPrio = PRIO_MDM;
+}
+
+function prioButtonLow(){
+    prioButtonUpdate(false, 'urgent-btn');
+    prioButtonUpdate(false, 'medium-btn');
+    prioButtonUpdate(true, 'low-btn');
+    taskPrio = PRIO_LOW;
+}
+
