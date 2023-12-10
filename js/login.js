6988a598215662d8742f98aa4c1755154951acb9
andre <andre.karyoti.schuro@gmail.com>
2023-12-10 23:51:12 +0100

    added some login logic | an issue with lookIfUserAllreadyExists()

--- js/login.js
+++ js/login.js
@@ -1,21 +1,74 @@
-let display = document.getElementById('sign-in-up_card');
-let frame = document.getElementById('sign-in-up-card_frame');
-let card = document.getElementById('sign-in-up_card');
+/* user array */
+let users = [
+    {
+        name: 'Hubertus',
+        email: 'test123@test.de',
+        password: 'test123'
+    },
+];
 
-function renderLogIn() {
-    frame.classList.add('login-card-frame');
-    frame.classList.remove('signUp-card-frame');
-    card.classList.add('login-card');
-    card.classList.remove('signUp-card');
-    display.innerHTML = generateLogInHTML();
+
+/* register */
+function registerNewUser() {
+    let password = document.getElementById('register_password-input');
+    let confirm = document.getElementById('register_confirm-input');
+    let name = document.getElementById('register_name-input');
+    let email = document.getElementById('register_email-input');
+
+    if (password.value === confirm.value && lookIfUsersAllreadyExists(email) === false) {
+        let newUser = generateNewUserArray(name.value, email.value, password.value);
+        users.push(newUser);
+    } else {
+        alert('das nicht funktionieren funktioniert');
+    }
 }
 
-function renderSignUp() {
-    frame.classList.add('signUp-card-frame');
-    frame.classList.remove('login-card-frame');
-    card.classList.add('signUp-card');
-    card.classList.remove('login-card');
-    display.innerHTML = generateSignUpHTML();
+function generateNewUserArray(name, email, password) {
+    return {
+        name: `${name}`,
+        email: `${email}`,
+        password: `${password}`
+    }
+}
+
+function lookIfUsersAllreadyExists(email) {
+    const foundUser = users.find(user => user.email.toLowerCase() === `${email}`.toLowerCase());
+    if (foundUser) {
+        console.log('found:' + foundUser);
+        return true;
+    } else {
+        console.log(foundUser);
+        return false;
+    }
+}
+
+/* handle signIn signUp */
+let signInDisplay = document.getElementById('login-card_frame');
+let signUpDisplay = document.getElementById('sign-up-card_frame');
+let paragraph = document.getElementById('switch-signIn-signUp_paragraph');
+let button = document.getElementById('handle-signIn-signUp_button');
+
+function handleSignInSignUp() {
+    if (!signInDisplay.classList.contains('d-none')) {
+        switchToSignUp();
+    } else {
+        switchToLogIn();
+        document.getElementById('accaptPolicy').checked = false;
+    }
+}
+
+function switchToSignUp() {
+    signInDisplay.classList.add('d-none');
+    signUpDisplay.classList.remove('d-none');
+    paragraph.innerHTML = `Allready have an Account?`;
+    button.innerHTML = `Log in`;
+}
+
+function switchToLogIn() {
+    signInDisplay.classList.remove('d-none');
+    signUpDisplay.classList.add('d-none');
+    paragraph.innerHTML = `Not a Join user?`;
+    button.innerHTML = `Sign up`;
 }
 
 function handleSignUpButton() {
@@ -27,83 +80,7 @@ function handleSignUpButton() {
     }
 }
 
-/* generate HTML */
-function generateLogInHTML() {
-    return `
-    <!-- headline -->
-    <div class="headline-frame dp-flex flex-column">
-        <h1 style="text-align: center;">Log in</h1>
-        <div class="headline-underline"></div>
-    </div>
-    
-
-    <!-- login inputs -->
-    <form style="gap: 32px;" class="dp-flex flex-column" action="#">
-        <div style="width: 422px;" class="input-container">
-            <input required class="input-field" type="email" placeholder="Email" />
-            <img src="../assets/img/icons/mail.svg" alt="mail" />
-        </div>
-        <div style="width: 422px;" class="input-container">
-            <input required type="password" class="input-field" type="email" placeholder="Password" />
-            <img src="../assets/img/icons/lock.svg" alt="mail" />
-        </div>
-        <div class="rememberMe dp-flex">
-            <input type="checkbox" id="remember" name="remember" value="remember-login">
-            <label for="remember">remember me</label><br>
-        </div>
-        <div class="login-button_frame dp-flex">
-            <button type="sumbit" class="button">Log in</button>
-            <div class="button-secondary">Guest Log in</div>
-        </div>
-   </form> 
-    `;
-}
-
-function generateSignUpHTML() {
-    return `
-    <div style="gap: 32px;" class="dp-flex flex-column">
-        <!-- headline -->
-        <div class="headline-frame dp-flex flex-column">
-            <h1 style="text-align: center; margin: 0; margin-bottom: 16px;">Sign up</h1>
-            <div class="headline-underline"></div>
-        </div>
-
-        <!-- form -->
-        <form class="dp-flex flex-column" action="#">
-            <div style="gap: 24px;" class="input-frame dp-flex flex-column">
-                <!-- name -->
-                <div style="width: 422px;" class="input-container">
-                    <input required class="input-field" type="text" placeholder="name">
-                    <img src="../assets/img/icons/mail.svg" alt="name">
-                </div>
-                <!-- email -->
-                <div style="width: 422px;" class="input-container">
-                    <input required class="input-field" type="email" placeholder="email">
-                    <img src="../assets/img/icons/mail.svg" alt="mail">
-                </div>
-                <!-- password -->
-                <div style="width: 422px;" class="input-container">
-                    <input required class="input-field" type="password" placeholder="password">
-                    <img src="../assets/img/icons/mail.svg" alt="password">
-                </div>
-                <!-- password confirm -->
-                <div style="width: 422px;" class="input-container">
-                    <input required class="input-field" type="password" placeholder="confirm password">
-                    <img src="../assets/img/icons/mail.svg" alt="confirm password">
-                </div>
-            </div>
-
-            <div class="dp-flex"><input style="margin-right: 6px;" type="checkbox" onclick="handleSignUpButton()">
-                <p>I accept the <a class="link" href="#">Privacy policy</a></p>
-            </div>
-
-            <button id="signUp-button" class="button d-none" type="submit">Sign up</button>
-        </form>
-    </div>
-    `;
-}
-
-/* eventlistener */
+/* eventlistener for animation */
 let animationsHelper = document.getElementById('animations-helper');
 
 animationsHelper.addEventListener('animationend', () => {
