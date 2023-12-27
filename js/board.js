5d3bd0d705c348c0b865d482b6bf40b69a81d075
Preston Jones <preston.jones@gmx.de>
2023-12-27 23:03:49 +0100

    drag and drop

--- js/board.js
+++ js/board.js
@@ -1,5 +1,11 @@
 let allTasksFromStorage = [];
-let currentDraggedElement;
+let currentDraggedElementID;
+let currentDraggedElementINDEX;
+
+let toDo;
+let inProgress;
+let awaitFeedback;
+let done;
 
 async function init() {
     await includeHTML();
@@ -10,6 +16,7 @@ async function init() {
 
 
 function openAddTaskPopup() {
+    renderAddTaskForm('add-task-placeholder');
     let popupContainer = document.getElementById('add_task_popup');
     document.getElementById('add_task_popup_container').classList.add('show_add_task_popup');
 }
@@ -23,7 +30,6 @@ function closeAddTaskPopup() {
 
 async function getTasks() {
     allTasksFromStorage = JSON.parse(await getItem("tasks"));
-    console.log(allTasksFromStorage);
 }
 
 
@@ -32,64 +38,75 @@ function loadBoard() {
     renderInProgress();
     renderAwaitFeedback();
     renderDone();
+    console.log(allTasksFromStorage);
 }
 
 
 function renderToDo() {
-    let toDo = allTasksFromStorage.filter(t => t['taskStatus'] === 0);
     document.getElementById('column_todo').innerHTML = '';
+    toDo = allTasksFromStorage.filter(t => t['taskStatus'] == 0);
+    console.log(toDo);
 
-    for (let index = 0; index < toDo.length; index++) {
-        const element = toDo[index];
-console.log(element);
-        document.getElementById('column_todo').innerHTML += thumbnailCardHTML(element, index);
+    if (toDo.length == 0) {
+        renderNoTaskToDo('column_todo');
+    }
+    else {
+        for (let index = 0; index < toDo.length; index++) {
+            const element = toDo[index];
+            renderThumbnailCard('column_todo', index, element)
+        }
     }
 }
 
 
 function renderInProgress() {
-    let inProgress = allTasksFromStorage.filter(t => t['taskStatus'] === 1);
     document.getElementById('column_in_progress').innerHTML = '';
+    inProgress = allTasksFromStorage.filter(t => t['taskStatus'] == 1);
 
     for (let index = 0; index < inProgress.length; index++) {
         const element = inProgress[index];
-        document.getElementById('column_in_progress').innerHTML += thumbnailCardHTML(element, index);
+        renderThumbnailCard('column_in_progress', index, element)
     }
 }
 
 
 function renderAwaitFeedback() {
-    let awaitFeedback = allTasksFromStorage.filter(t => t['taskStatus'] === 2);
     document.getElementById('column_await_feedback').innerHTML = '';
+    awaitFeedback = allTasksFromStorage.filter(t => t['taskStatus'] == 2);
 
     for (let index = 0; index < awaitFeedback.length; index++) {
         const element = awaitFeedback[index];
-        document.getElementById('column_await_feedback').innerHTML += thumbnailCardHTML(element, index);
+        renderThumbnailCard('column_await_feedback', index, element)
     }
 }
 
 
 function renderDone() {
-    let done = allTasksFromStorage.filter(t => t['taskStatus'] === 3);
     document.getElementById('column_done').innerHTML = '';
+    done = allTasksFromStorage.filter(t => t['taskStatus'] == 3);
 
     for (let index = 0; index < done.length; index++) {
         const element = done[index];
-        document.getElementById('column_done').innerHTML += thumbnailCardHTML(element, index);
+        renderThumbnailCard('column_done', index, element)
     }
 }
 
 
 function renderAssignedUsers(index) {
     for (let i = 0; i < allTasksFromStorage[index].assignedUsers.length; i++) {
-        console.log(allTasksFromStorage[index].assignedUsers[i].bgColor);
         document.getElementById('task_card_thumbnail_assigned_users_container').innerHTML +=
             `<div class="task_card_thumbnail_profile_badge_frame" style="background-color: ${allTasksFromStorage[index].assignedUsers[i].bgColor};">
             <div class="task_card_thumbnail_profile_badge">AB</div>
             </div>`;
     }
-    
+
 }
+
+
+// function findIndexOfTaskById(arrayToSearch, Id) {
+//     let index = arrayToSearch.findIndex(item => item.taskID === Id);
+//     return index;
+// }
 
 
 
@@ -99,15 +116,18 @@ function allowDrop(ev) {
 }
 
 
-function startDragging(id, index) {
-    currentDraggedElement = index;
-    console.log(id);
-    console.log(index);
+function startDragging(index, element_taskID) {
+    currentDraggedElementID = element_taskID;
+    currentDraggedElementINDEX = index;
+    console.log(element_taskID);
 }
 
 
 function moveTo(task_status) {
-    allTasksFromStorage[currentDraggedElement].taskStatus = task_status;
+    let currentDraggedElement = allTasksFromStorage.filter(t => t['taskID'] == currentDraggedElementID);
+    console.log(currentDraggedElement);
+    currentDraggedElement[0]['taskStatus'] = task_status;
+    setItem('tasks', allTasksFromStorage);
     loadBoard();
 }
 
@@ -125,9 +145,8 @@ function removeHighlight(id) {
 // HTML Templates
 
 
-function generateNoTaskToDo() {
-    let columnToDo = document.getElementById('column_todo');
-    columnToDo.innerHTML += noTaskToDoHTML();
+function renderNoTaskToDo(category) {
+    document.getElementById(category).innerHTML += noTaskToDoHTML();
 }
 
 
@@ -138,29 +157,44 @@ function noTaskToDoHTML() {
 }
 
 
-function thumbnailCardHTML(element, index) {
-    return ` <div id="${element.taskID}" class="task-card-thumbnail-container" draggable="true" ondragstart="startDragging(${element.taskID},${index})">
+function renderThumbnailCard(category, index, element) {
+    document.getElementById(category).innerHTML += thumbnailCardHTML(index, element);
+    renderAssignedUsers(element);
+}
+
+
+function thumbnailCardHTML(index, element) {
+    return ` <div id="${element.taskID}" class="task-card-thumbnail-container" draggable="true" ondragstart="startDragging(${index}, ${element.taskID})">
     <div class="task_card_thumbnail_content">
-        <div class="task_card_thumbnail_label" style="background: ${allTasksFromStorage[index].category.colour};">
-            ${allTasksFromStorage[index].category.name}
+        <div class="task_card_thumbnail_label" style="background: ${element.category.colour};">
+            ${element.category.name}
         </div>
         <div class="task_card_thumbnail_main">
-            <div class="task_card_thumbnail_title">${allTasksFromStorage[index].title}</div>
-            <div class="task_card_thumbnail_description">${allTasksFromStorage[index].description}</div>
+            <div class="task_card_thumbnail_title">${element.title}</div>
+            <div class="task_card_thumbnail_description">${element.description}</div>
         </div>
         <div class="task_card_thumbnail_progress">
             <div class="task_card_thumbnail_progressbar_container">
                 <div class="task_card_thumbnail_progressbar" style="width: 50%;"></div>
             </div>
-            <div>0/${allTasksFromStorage[index].subtasks.length} Subtasks</div>
+            <div>0/${element.subtasks.length} Subtasks</div>
         </div>
         <div class="task_card_thumbnail_prio_and_assignment">
-            <div class="task_card_thumbnail_assigned_users_container" id="task_card_thumbnail_assigned_users_container">
+            <div class="task_card_thumbnail_assigned_users_container" id="task_card_thumbnail_assigned_users_container_${element.taskID}">
             </div>
             <div class="task_card_thumbnail_assigned_priority_symbol_container">
-                <img class="task_card_thumbnail_assigned_priority_symbol" id="task_card_thumbnail_assigned_priority_symbol" src="assets/img/icons/prio${allTasksFromStorage[index].prio}.svg">
+                <img class="task_card_thumbnail_assigned_priority_symbol" id="task_card_thumbnail_assigned_priority_symbol" src="assets/img/icons/prio${element.prio}.svg">
             </div>
         </div>
     </div>
-</div>`
+</div>`;
+}
+
+
+function renderAssignedUsers(element) {
+    for (let i = 0; i < element.assignedUsers.length; i++) {
+        document.getElementById(`task_card_thumbnail_assigned_users_container_${element.taskID}`).innerHTML += `
+            ${element.assignedUsers[i].name} / 
+        `;
+    }
 }
