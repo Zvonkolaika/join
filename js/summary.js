a95e8c851e4b709a19d908441949bf6ea70316f9
Florian Hoffmann <florian.hoffmann@mailbox.org>
2024-01-10 15:24:11 +0100

    fixed bugs and added js docs on summary and contacts pages, moved backgroundColors array from contacts.js to js/backgroundColors.js

--- js/summary.js
+++ js/summary.js
@@ -1,23 +1,35 @@
 let tasks = [];
+let prioTasks = [];
 
+/**
+ * This function loads several functions required on page load
+ */
 async function init() {
   await loadTemplates();
   await loadTasks();
+  convertDueDates();
+  loadPrioTasks();
   greetUser();
   renderMetricValues();
   setCurrentPageLinkActive("summary");
 }
 
+/**
+ * This function loads all tasks objects from the
+ * remote storage - using getItem function - and saves them to the tasks array
+ */
 async function loadTasks() {
   try {
     tasks = JSON.parse(await getItem("tasks"));
-    convertDueDates();
-    console.log(`${tasks.length} tasks successfully loaded`, tasks);
   } catch (e) {
     console.warn("no tasks found on server");
   }
 }
 
+/**
+ * This function parses through the tasks array and converts the
+ * dueDates from string to new Date() format
+ */
 function convertDueDates() {
   for (let i = 0; i < tasks.length; i++) {
     let taskDueDate = new Date(tasks[i]["date"]);
@@ -25,21 +37,47 @@ function convertDueDates() {
   }
 }
 
-function returnNumberofTasks(key, value) {
+/**
+ * This function parses throuh the tasks array to return
+ * the number of objects matching the criteria set by the parameters
+ * @param {string} property - parameter to specify the objects property
+ * @param {string} PropertyValue - parameter to specify the property value
+ * @returns {number} the number of objects matching the criteria set by the parameters
+ */
+function returnNumberOfTasks(property, PropertyValue) {
   let x = 0;
   for (let i = 0; i < tasks.length; i++) {
     const task = tasks[i];
-    if (task[key] == value) x++;
+    if (task[property] == PropertyValue) x++;
   }
   return x;
 }
 
+/**
+ * This function parses throuh the tasks array
+ * @returns {number} - total number of objects
+ */
 function returnTotalTasks() {
   return tasks.length;
 }
 
+/**
+ * This funtion parses through the tasks array
+ * to push all objects with high priority to the prioTasks array
+ */
+function loadPrioTasks() {
+  for (let i = 0; i < tasks.length; i++) {
+    const task = tasks[i];
+    if (task["prio"] == 1) prioTasks.push(task);
+  }
+}
+
+/**
+ * This function parses through the prioTasks array and
+ * @returns {string} the next upcoming due date
+ */
 function returnNextDueDate() {
-  const dueDate = new Date(Math.min(...tasks.map((e) => e.date)));
+  const dueDate = new Date(Math.min(...prioTasks.map((e) => e.date)));
 
   if (!isNaN(dueDate)) {
     const dueDateString = dueDate.toLocaleString("default", {
@@ -53,29 +91,36 @@ function returnNextDueDate() {
   }
 }
 
+/**
+ * This function calls the returnNumberOfTasks() function to fill
+ * the innerHTML with the relevant metrics to be displayed
+ */
 function renderMetricValues() {
-  document.getElementById("todo").innerHTML = returnNumberofTasks(
+  document.getElementById("todo").innerHTML = returnNumberOfTasks(
     "taskStatus",
     0
   );
-  document.getElementById("done").innerHTML = returnNumberofTasks(
+  document.getElementById("done").innerHTML = returnNumberOfTasks(
     "taskStatus",
     3
   );
-  document.getElementById("priority").innerHTML = returnNumberofTasks(
+  document.getElementById("priority").innerHTML = returnNumberOfTasks(
     "prio",
     1
   );
   document.getElementById("deadline").innerHTML = returnNextDueDate();
   document.getElementById("total-tasks").innerHTML = returnTotalTasks();
-  document.getElementById("in-progress-tasks").innerHTML = returnNumberofTasks(
+  document.getElementById("in-progress-tasks").innerHTML = returnNumberOfTasks(
     "taskStatus",
     1
   );
   document.getElementById("awaiting-feedback-tasks").innerHTML =
-    returnNumberofTasks("taskStatus", 2);
+    returnNumberOfTasks("taskStatus", 2);
 }
 
+/**
+ * This function greets the user based on the logged in user and time of the day
+ */
 function greetUser() {
   let now = new Date();
   let time = now.getHours();
