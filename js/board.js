3dcd707097ffde64ac08e68a65c6278cb20d4f5e
Preston Jones <preston.jones@gmx.de>
2024-01-11 17:57:53 +0100

    commit

--- js/board.js
+++ js/board.js
@@ -194,37 +194,9 @@ function noTaskDoneHTML() {
 
 
 function renderThumbnailCard(category, index, element) {
-    document.getElementById(category).innerHTML += thumbnailCardHTML(index, element);
+    document.getElementById(category).innerHTML += thumbnailCard_HTML(index, element);
     renderAssignedUsers(element);
     loadSubtasksInThumbnail(element);
-}
-
-
-function thumbnailCardHTML(index, element) {
-    return ` <div id="${element.taskID}" class="task-card-thumbnail-container" draggable="true" ondragstart="startDragging(${index}, ${element.taskID})">
-    <div class="task_card_thumbnail_content"  onclick="openTaskCard('task-card', ${element.taskID})">
-        <div class="task_card_thumbnail_label" style="background: ${element.category.colour};">
-            ${element.category.name}
-        </div>
-        <div class="task_card_thumbnail_main">
-            <div class="task_card_thumbnail_title">${element.title}</div>
-            <div class="task_card_thumbnail_description">${element.description}</div>
-        </div>
-        <div class="task_card_thumbnail_progress" id="task_card_thumbnail_progress_${element.taskID}">
-            <div class="task_card_thumbnail_progressbar_container">
-                <div class="task_card_thumbnail_progressbar" id="task_card_thumbnail_progressbar" style="width: ${renderSubtaskProgressBar(element.subtasks)}%;"></div>
-            </div>
-            <div>${SubtasksDone(element.subtasks)}/${element.subtasks.length} Subtasks</div>
-        </div>
-        <div class="task_card_thumbnail_prio_and_assignment">
-            <div class="task_card_thumbnail_assigned_users_container" id="task_card_thumbnail_assigned_users_container_${element.taskID}">
-            </div>
-            <div class="task_card_thumbnail_assigned_priority_symbol_container">
-                <img class="task_card_thumbnail_assigned_priority_symbol" id="task_card_thumbnail_assigned_priority_symbol" src="assets/img/icons/prio${element.prio}.svg">
-            </div>
-        </div>
-    </div>
-</div>`;
 }
 
 
@@ -248,57 +220,7 @@ async function renderTaskCardBoard(elementId, cardID) {
     await getTasks();
     task = allTasksFromStorage.filter(t => t['taskID'] == cardID)[0];
     let taskCard = document.getElementById(elementId);
-    taskCard.innerHTML = `
-    <div class="task_card_content" id = "task_card_ID_${task['taskID']}">
-    <div class="task_card_header">
-        <div class="task_card_thumbnail_label"  style="background: ${task['category']['colour']};">
-            <span>${task['category']['name']}</span>
-        </div>
-        <div class="task_card_header_close_icon_frame" onclick="closeTaskCard()">
-        <img src="assets/img/icons/cancel.svg">
-        </div>
-    </div>
-
-    <div class="task_card_title">
-        <h1>${task['title']}</h1>
-    </div>
-
-    <div class="task_card_description">
-        <span>${task['description']}</span>
-    </div>
-
-    <div class="task_card_date">
-        <span>Due date: </span>${normalDate(task['date'])}
-    </div>
-    <div class="task_card_prio">
-        <span>Priority: </span>${renderPriority(task['prio'], true)}
-    </div>
-
-    <div class="task_card_assigned_users_container">
-        <span>Assigned to:</span>
-        <div id="assigned-user-icons-container">
-            ${renderAssignedUserIcons(task['assignedUsers'])}
-        </div>       
-    </div>
-
-    <div class="task_card_subtask_container">
-        <span>Subtasks</span>
-        <div class="task_card_subtask_list" id="subtask-container-list-${task['taskID']}">
-        ${loadSubtasksInCard(task.taskID, task.subtasks)}
-        </div>
-    </div>
-    <div class="create-delete-task-container-responsive">
-        <button id="task-delete" type="reset" onclick="deleteTask(${task['taskID']}); showPopUp('${elementId}', false)">
-            <img src="./assets/img/icons/delete.svg" />
-            Delete
-            </button>
-        <button id="task-edit" type="submit" onclick="loadEditTaskForm('${elementId}', ${task['taskID']})">
-        <img src="./assets/img/icons/edit.svg" />
-        Edit
-        </button>
-    </div>
-</div >
-                `;
+    taskCard.innerHTML = taskCardBoard_HTML(task, elementId);
 }
 
 
@@ -314,7 +236,7 @@ function loadSubtasksInCard(taskID, subtasks) {
 
 function loadSubtasksInThumbnail(element) {
     if (element.subtasks.length == 0) {
-        document.getElementById(`task_card_thumbnail_progress_${ element.taskID }`).innerHTML = `< div class="no_subtasks" > No Subtasks</div >`;
+        document.getElementById(`task_card_thumbnail_progress_${ element.taskID }`).innerHTML = `<div class="no_subtasks">No Subtasks</div>`;
     }
 }
 
