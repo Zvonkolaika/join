5d7e98dcdede4dcdfa9c2e58da1c60c39967ccd8
Florian Hoffmann <florian.hoffmann@mailbox.org>
2024-01-16 17:51:18 +0100

    added privacy content

--- js/privacyLegality.js
+++ js/privacyLegality.js
@@ -1,43 +1,39 @@
 /* init */
-document.addEventListener('DOMContentLoaded', async () => {
-    await loadTemplates();
-    lookIfLoginParameterIsInLink();
-    renderUsersMailAddressIntoLegalNotice()
+document.addEventListener("DOMContentLoaded", async () => {
+  await loadTemplates();
+  lookIfLoginParameterIsInLink();
 });
 
 /*-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 
 /* functions to display visibility from privacy & legal note links, in the sidebar, depending from wich site you came */
 function lookIfLoginParameterIsInLink() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const login = urlParams.get('login');
+  const urlParams = new URLSearchParams(window.location.search);
+  const login = urlParams.get("login");
 
-    if (login === 'login') {
-        hideLinks();
-        removeHrefFromLinks();
-        document.getElementById('privacy-link').removeAttribute('onclick');
-        document.getElementById('legality-link').removeAttribute('onclick');
-        document.getElementById('privacy-link').setAttribute('onclick', 'cameFromLogin(`privacy`)');
-        document.getElementById('legality-link').setAttribute('onclick', 'cameFromLogin(`legality`)');
-    }
+  if (login === "login") {
+    hideLinks();
+    removeHrefFromLinks();
+    document.getElementById("privacy-link").removeAttribute("onclick");
+    document.getElementById("legality-link").removeAttribute("onclick");
+    document
+      .getElementById("privacy-link")
+      .setAttribute("onclick", "cameFromLogin(`privacy`)");
+    document
+      .getElementById("legality-link")
+      .setAttribute("onclick", "cameFromLogin(`legality`)");
+  }
 }
 
 function hideLinks() {
-    document.getElementById('main-nav-links').classList.add('d-none');
+  document.getElementById("main-nav-links").classList.add("d-none");
 }
 
 function cameFromLogin(p) {
-    window.location.href = `${p}.html?login=login`;
+  window.location.href = `${p}.html?login=login`;
 }
 
 function removeHrefFromLinks() {
-    document.getElementById('privacy-link').setAttribute('href', '#');
-    document.getElementById('legality-link').setAttribute('href', '#');
-}
-
-/*-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
-
-/* Render the mail address from exploring user into legal notice */
-function renderUsersMailAddressIntoLegalNotice() {
-    document.getElementById('users-email').innerHTML = `email: ${sessionStorage.getItem('user-mail')}`;
+  document.getElementById("privacy-link").setAttribute("href", "#");
+  document.getElementById("legality-link").setAttribute("href", "#");
 }
